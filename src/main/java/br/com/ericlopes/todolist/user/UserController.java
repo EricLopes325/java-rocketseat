8a19f62fdac98fdc97fdf1d9466ce9a83f4460d5
Eric Lopes da Silva <lopeseric325@gmail.com>
2025-06-25 15:55:38 -0300

    Lombok
    Banco de dados, Criando tabela de usuário e Repository

--- src/main/java/br/com/ericlopes/todolist/user/UserController.java
+++ src/main/java/br/com/ericlopes/todolist/user/UserController.java
@@ -3,6 +3,7 @@ package br.com.ericlopes.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -25,10 +26,14 @@ public class UserController {
      * char (A C)
      * Date (data)
      */
+
+     @Autowired
+     private IUserRepository userRepository;
     
 
      @PostMapping("/")
-     public void create (@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+     public UserModel create (@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
      }
 }
